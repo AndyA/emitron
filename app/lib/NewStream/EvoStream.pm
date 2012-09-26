47ea79a0e17ccd61ec9179aa3e1405ad40e374eb
Andy Armstrong <andy@hexten.net>
2012-09-27 00:12:43 +0200

    API module.

---
+++ app/lib/NewStream/EvoStream.pm
@@ -0,0 +1,89 @@
+package NewStream::EvoStream;
+
+use strict;
+use warnings;
+
+use Data::Dumper;
+use JSON::XS;
+use LWP::UserAgent;
+use MIME::Base64;
+
+use accessors::ro qw( host port );
+
+BEGIN {
+  my %API = (
+    add_stream_alias                 => 'addStreamAlias',
+    create_hLSStream                 => 'createHLSStream',
+    create_service                   => 'createService',
+    enable_service                   => 'enableService',
+    flush_stream_aliases             => 'flushStreamAliases',
+    get_bandwidth                    => 'getBandwidth',
+    get_connection_info              => 'getConnectionInfo',
+    get_connections_count            => 'getConnectionsCount',
+    get_connections_count_limit      => 'getConnectionsCountLimit',
+    get_extended_connection_counters => 'getExtendedConnectionCounters',
+    get_stream_info                  => 'getStreamInfo',
+    get_streams_count                => 'getStreamsCount',
+    help                             => 'help',
+    list_connections                 => 'listConnections',
+    list_connections_ids             => 'listConnectionsIds',
+    list_pull_push_config            => 'listPullPushConfig',
+    list_services                    => 'listServices',
+    list_stream_aliases              => 'listStreamAliases',
+    list_streams                     => 'listStreams',
+    list_streams_ids                 => 'listStreamsIds',
+    pull_stream                      => 'pullStream',
+    push_stream                      => 'pushStream',
+    quit                             => 'quit',
+    record                           => 'record',
+    remove_pull_push_config          => 'removePullPushConfig',
+    remove_stream_alias              => 'removeStreamAlias',
+    reset_max_fd_counters            => 'resetMaxFdCounters',
+    reset_total_fd_counters          => 'resetTotalFdCounters',
+    set_authentication               => 'setAuthentication',
+    set_bandwidth_limit              => 'SetBandwidthLimit',
+    set_connections_count_limit      => 'setConnectionsCountLimit',
+    set_log_level                    => 'setLogLevel',
+    shutdown_server                  => 'shutdownServer',
+    shutdown_service                 => 'shutdownService',
+    shutdown_stream                  => 'shutdownStream',
+    version                          => 'version',
+  );
+  while ( my ( $method, $api ) = each %API ) {
+    no strict 'refs';
+    *$api = sub { shift->api( $api, @_ ) };
+  }
+}
+
+=head1 NAME
+
+NewStream::EvoStream - EvoStream Media Server API
+
+=cut
+
+sub new {
+  my ( $class, %args ) = @_;
+  return bless { %args, host => 'localhost', port => '7777' }, $class;
+}
+
+sub ua {
+  my $self = shift;
+  return $self->{ua} ||= LWP::UserAgent->new;
+}
+
+sub api {
+  my ( $self, $function, %args ) = @_;
+  my $uri
+   = 'http://' . $self->host . ':' . $self->port . '/' . $function;
+  if ( keys %args ) {
+    my $args = join ' ', map { "$_=$args{$_}" } sort keys %args;
+    $uri .= '?params=' . encode_base64( $args, '' );
+  }
+  my $resp = $self->ua->get( $uri );
+  die $resp->status_line if $resp->is_error;
+  return decode_json $resp->content;
+}
+
+1;
+
+# vim:ts=2:sw=2:sts=2:et:ft=perl
--- tools/evostream.pl
+++ tools/evostream.pl
@@ -3,35 +3,14 @@
 use strict;
 use warnings;
 
+use lib qw( app/lib );
+
+use NewStream::EvoStream;
+
 use Data::Dumper;
-use JSON::XS;
-use LWP::UserAgent;
-use MIME::Base64;
 
-my $evo = evo->new( host => 'localhost' );
-print Dumper( $evo->api( 'version' ) );
-
-sub evo::new {
-  my ( $class, %args ) = @_;
-  return bless {%args}, $class;
-}
-
-sub evo::ua {
-  my $self = shift;
-  return $self->{ua} ||= LWP::UserAgent->new;
-}
-
-sub evo::api {
-  my ( $self, $function, %args ) = @_;
-  my $uri = 'http://' . $self->{host} . ':7777/' . $function;
-  if ( keys %args ) {
-    my $args = join ' ', map { "$_=$args{$_}" } sort keys %args;
-    $uri .= '?params=' . encode_base64( $args, '' );
-  }
-  my $resp = $self->ua->get( $uri );
-  die $resp->status_line if $resp->is_error;
-  return decode_json $resp->content;
-}
+my $evo = NewStream::EvoStream->new( host => 'localhost' );
+print Dumper( $evo->version );
 
 # vim:ts=2:sw=2:sts=2:et:ft=perl
 
