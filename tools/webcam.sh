f0900ace16a00eeee60e224753f7bccc18ceff6c
Andy Armstrong <andy@hexten.net>
2012-10-17 00:24:59 +0100

    Convert to stereo.

--- tools/webcam.sh
+++ tools/webcam.sh
@@ -4,8 +4,8 @@
 ffmpeg -y \
   -f alsa -ac 1 -r:a 48000 -i hw:1,0 \
   -f video4linux2 -i /dev/video0 \
-  -acodec libfaac -b:a 128k \
+  -acodec libfaac -b:a 128k -ac 2 \
   -vcodec libx264 -b:v 2000k -r:v 25 -bf 0 \
-  -threads 0 -f flv rtmp://newstream.fenkle/live/phool
+  -threads 0 -f flv rtmp://localhost/live/phool
 
 # vim:ts=2:sw=2:sts=2:et:ft=sh
