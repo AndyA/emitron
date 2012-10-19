f5b61f54ae718e3ae4aff6a361864244f1b127f1
Andy Armstrong <andy@hexten.net>
2012-10-19 13:17:50 +0100

    Merge changes.

--- tools/webcam.sh
+++ tools/webcam.sh
@@ -2,6 +2,9 @@
 
 #      -bsf:a aac_adtstoasc
 
+remote=newstream.fenkle
+me=$( hostname -s )
+url=rtmp://$remote/live/$me
 
 set -x
 v4l2-ctl -p 15
@@ -12,6 +15,6 @@ ffmpeg -y \
   -acodec libfaac -b:a 128k -ac 2 \
   -vcodec libx264 -b:v 2000k \
   -profile:v baseline -bf 0 -g 100 -keyint_min 50 -r:v 15 \
-  -threads 0 -f flv rtmp://localhost/live/phool
+  -threads 0 -f flv "$url"
 
 # vim:ts=2:sw=2:sts=2:et:ft=sh
