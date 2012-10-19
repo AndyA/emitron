dab3d5de90ad2f0b6086ae93ab17cf3cf9ddaaad
Andy Armstrong <andy@hexten.net>
2012-10-19 13:18:25 +0100

    Set rate.

--- tools/webcam.sh
+++ tools/webcam.sh
@@ -5,12 +5,13 @@
 remote=newstream.fenkle
 me=$( hostname -s )
 url=rtmp://$remote/live/$me
+rate=15
 
 set -x
-v4l2-ctl -p 15
+v4l2-ctl -p $rate
 ffmpeg -y \
   -f alsa -ac 1 -r:a 48000 -i hw:1,0 \
-  -f video4linux2 -r:v 15 -i /dev/video0 \
+  -f video4linux2 -r:v $rate -i /dev/video0 \
   -vf 'format=yuv420p' \
   -acodec libfaac -b:a 128k -ac 2 \
   -vcodec libx264 -b:v 2000k \
