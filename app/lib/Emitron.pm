8a4f1aa756ffca29c07746f07a2631e66c72189a
Andy Armstrong <andy@hexten.net>
2012-10-29 20:56:13 +0000

    Return serialised JSON message.

--- app/lib/Emitron.pm
+++ app/lib/Emitron.pm
@@ -4,11 +4,12 @@ use Dancer ':syntax';
 our $VERSION = '0.1';
 
 get '/' => sub {
-    template 'index';
+  template 'index';
 };
 
 get '/api/ev' => sub {
-
+  sleep 5;
+  return { name => 'test', data => { foo => 1 } };
 };
 
 true;
