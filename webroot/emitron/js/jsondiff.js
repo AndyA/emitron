b4e78a94f32fb7b00e424f2bfcca5136967890b8
Andy Armstrong <andy@hexten.net>
2012-11-01 17:59:54 +0000

    Working JSONDiff.

--- webroot/emitron/js/jsondiff.js
+++ webroot/emitron/js/jsondiff.js
@@ -2,4 +2,115 @@ function JSONDiff() {
 
 }
 
-JSONDiff.prototype = {}
+JSONDiff.prototype = (function() {
+  function type(obj) {
+    if (obj == null) return 'null';
+    if (obj.substring || obj.toFixed) return 'scalar';
+    if (obj instanceof Array) return 'array';
+    return 'object';
+  }
+
+  function keys(obj) {
+    var k = [];
+    for (var i in obj) if (obj.hasOwnProperty(i)) k.push(i);
+    return k;
+  }
+
+  function uniq() {
+    var seen = {};
+    var l = [];
+    for (var i = 0; i < arguments.length; i++) {
+      var ar = arguments[i];
+      for (var j = 0; j < ar.length; j++) {
+        var v = ar[j];
+        if (!seen.hasOwnProperty(v)) {
+          l.push(v);
+          seen[v] = true;
+        }
+      }
+    }
+    return l;
+  }
+
+  function diffwalk(da, db, path, cb) {
+    var ta = type(da);
+    var tb = type(db);
+    if (ta == tb) {
+      switch (ta) {
+      case 'null':
+        break;
+      case 'scalar':
+        if (da !== db) {
+          cb('remove', path);
+          cb('add', path, db);
+        }
+        break;
+      case 'array':
+        var mi = Math.min(da.length, db.length);
+        for (var i = 0; i < mi; i++) {
+          path.push(i);
+          diffwalk(da[i], db[i], path, cb);
+          path.pop();
+        }
+        for (i = da.length; i < db.length; i++) {
+          path.push(i);
+          cb('add', path, db[i]);
+          path.pop();
+        }
+        path.push(db.length);
+        for (i = db.length; i < da.length; i++) {
+          cb('remove', path);
+        }
+        path.pop();
+        break;
+      case 'object':
+        var k = uniq(keys(da), keys(db));
+        for (var i = 0; i < k.length; i++) {
+          var key = k[i];
+          path.push(key);
+          if (da.hasOwnProperty(key)) {
+            if (db.hasOwnProperty(key)) diffwalk(da[key], db[key], path, cb);
+            else cb('remove', path);
+          }
+          else {
+            cb('add', path, db[key]);
+          }
+          path.pop();
+        }
+        break;
+      }
+    }
+    else {
+      cb('remove', path);
+      cb('add', path, db);
+    }
+  }
+
+  return {
+    diff: function(da, db) {
+      var d = [];
+      diffwalk(da, db, ['$'], function(verb, path, value) {
+        switch (verb) {
+        case 'add':
+          var p = path.slice();
+          var e = p.pop();
+          var rec = {
+            op: 'add',
+            element: e,
+            value: value
+          };
+          if (p.length) rec.path = p.join('.');
+          d.push(rec);
+          break;
+        case 'remove':
+          d.push({
+            op: 'remove',
+            path: path.join('.')
+          });
+          break;
+        }
+      });
+      return d;
+    }
+  };
+})();
--- webroot/emitron/test/t/jsondiff.js
+++ webroot/emitron/test/t/jsondiff.js
@@ -1,5 +1,124 @@
 // Tests for JSONDiff
 module("JSONDiff");
-test("basic", function() {
-  ok(true, 'Good!');
+
+test("diff", function() {
+  // TODO share data with jsonpatch
+  var data = [{
+    name: "Type change/hash",
+    a: {
+      bar: "two",
+      foo: 1
+    },
+    b: {
+      bar: [1, 2, 3],
+      foo: 1
+    },
+    diff: [{
+      path: "$.bar",
+      op: "remove"
+    },
+    {
+      value: [1, 2, 3],
+      path: "$",
+      element: "bar",
+      op: "add"
+    }]
+  },
+  {
+    name: "Hash",
+    a: {
+      bar: "two",
+      foo: 1
+    },
+    b: {
+      bar: "three",
+      foo: 1
+    },
+    diff: [{
+      path: "$.bar",
+      op: "remove"
+    },
+    {
+      value: "three",
+      path: "$",
+      element: "bar",
+      op: "add"
+    }]
+  },
+  {
+    name: "Array",
+    a: [1, 2, 3],
+    b: [1, 3, 2],
+    diff: [{
+      path: "$.1",
+      op: "remove"
+    },
+    {
+      value: 3,
+      path: "$",
+      element: 1,
+      op: "add"
+    },
+    {
+      path: "$.2",
+      op: "remove"
+    },
+    {
+      value: 2,
+      path: "$",
+      element: 2,
+      op: "add"
+    }]
+  },
+  {
+    name: "Root scalar",
+    a: "One",
+    b: 2,
+    diff: [{
+      path: "$",
+      op: "remove"
+    },
+    {
+      value: 2,
+      element: "$",
+      op: "add"
+    }]
+  },
+  {
+    name: "Add key",
+    a: {},
+    b: {
+      args: ["Snoofus?"]
+    },
+    diff: [{
+      value: ["Snoofus?"],
+      path: "$",
+      element: "args",
+      op: "add"
+    }]
+  },
+  {
+    name: "Empty array",
+    a: {
+      args: ["Hello", "World"]
+    },
+    b: {
+      args: []
+    },
+    diff: [{
+      path: "$.args.0",
+      op: "remove"
+    },
+    {
+      path: "$.args.0",
+      op: "remove"
+    }]
+  }];
+
+  expect(data.length);
+  for (var tn = 0; tn < data.length; tn++) {
+    var tc = data[tn];
+    var d = new JSONDiff().diff(tc.a, tc.b);
+    deepEqual(d, tc.diff, tc.name);
+  }
 });
