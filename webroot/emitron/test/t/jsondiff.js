0cccfac9438b9ef3bfbc78385078dc55b9e8e95e
Andy Armstrong <andy@hexten.net>
2012-11-02 13:04:21 +0000

    Assert that input data isn't modified.

--- webroot/emitron/test/t/jsondiff.js
+++ webroot/emitron/test/t/jsondiff.js
@@ -3,12 +3,13 @@ module("JSONDiff");
 
 asyncTest("diff", function() {
   TestData.withData('data/diffpatch.json', function(data) {
-    expect(data.length);
+    var clone = $.extend(true, [], data);
     for (var tn = 0; tn < data.length; tn++) {
       var tc = data[tn];
       var d = new JSONDiff().diff(tc.a, tc.b);
       deepEqual(d, tc.diff, tc.name);
     }
+    deepEqual(data, clone, "input unchanged");
     start();
   });
 });
