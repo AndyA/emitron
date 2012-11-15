63c1d7b4dd4369c8213f6b7f0c1e2aecfaffc1e1
Andy Armstrong <andy@hexten.net>
2012-11-15 12:48:38 +0000

    Lowercase variables. It was bugging me.

--- tools/hlslive.sh
+++ tools/hlslive.sh
@@ -3,86 +3,86 @@
 while getopts 'lbp' opt; do
   case $opt in
     b) 
-      BURNIN=1 
+      burnin=1 
       ;;
     l) 
-      LIVE=1
+      live=1
       ;;
     p)
-      PREPROCESS=1
+      preprocess=1
       ;;
   esac
 done
 shift $((OPTIND-1))
 
-INPUTFILE="$1"
-OUTPUTDIR="$2"
-if [ -z "$OUTPUTDIR" ]; then
+inputfile="$1"
+outputdir="$2"
+if [ -z "$outputdir" ]; then
   echo "Usage: $0 [-l] [-b] <infile> <outdir>" 1>&2
   exit 1
 fi
 
-rm -rf "$OUTPUTDIR"
-mkdir -p "$OUTPUTDIR"
-OUTPUTFILE="$OUTPUTDIR/$( basename "$OUTPUTDIR" )"
+rm -rf "$outputdir"
+mkdir -p "$outputdir"
+outputfile="$outputdir/$( basename "$outputdir" )"
 
-WORK="/tmp/hlslive.$$.work"
-mkdir -p "$WORK"
-LOGS="$WORK/logs"
-mkdir -p "$LOGS"
+work="/tmp/hlslive.$$.work"
+mkdir -p "$work"
+logs="$work/logs"
+mkdir -p "$logs"
 
-GOP=8
-PRESET=veryfast
-AUDIO_OPTIONS="-acodec libfaac -ac 2"
-VIDEO_OPTIONS="-vcodec libx264"
-VIDEO_EXTRA="-preset $PRESET -sc_threshold 0"
-PIPEFMT=mpegts
+gop=8
+preset=veryfast
+audio_options="-acodec libfaac -ac 2"
+video_options="-vcodec libx264"
+video_extra="-preset $preset -sc_threshold 0"
+pipefmt=mpegts
 
-FONT="$HOME/Dropbox/Fonts/Envy Code R.ttf"
+font="$HOME/Dropbox/Fonts/Envy Code R.ttf"
 
-#RATES="
+#rates="
 #  W=400;H=224;R=25;BV=300;BA=96;AR=44100;P=baseline
 #  W=640;H=360;R=25;BV=704;BA=96;AR=44100;P=main
 #  W=688;H=384;R=25;BV=1372;BA=128;AR=48000;P=main
 #  W=1024;H=576;R=25;BV=2000;BA=96;AR=44100;P=main"
 
-RATES="
+rates="
   W=400;H=224;R=25;BV=300;BA=96;AR=44100;P=baseline
   W=640;H=360;R=25;BV=704;BA=96;AR=44100;P=baseline
   W=688;H=384;R=25;BV=1372;BA=128;AR=48000;P=baseline
   W=1024;H=576;R=25;BV=2000;BA=96;AR=44100;P=baseline"
 
-FIFOS=""
-TEES=""
-TOKILL=""
+fifos=""
+tees=""
+tokill=""
 
 function _cleanup() {
-  rm -f $FIFOS
+  rm -f $fifos
 }
 
 function _shutdown() {
-  kill $TOKILL
+  kill $tokill
   _cleanup
 }
 
 trap _shutdown SIGINT
 
-SOURCE="$INPUTFILE"
-case $INPUTFILE in
+source="$inputfile"
+case $inputfile in
   rtsp://*)
-    FIFO="$WORK/input.fifo"
-    FIFOS="$FIFOS $FIFO"
-    LOG="$LOGS/gst.log"
-    mkfifo $FIFO
+    fifo="$work/input.fifo"
+    fifos="$fifos $fifo"
+    log="$logs/gst.log"
+    mkfifo $fifo
     {
       gst-launch \
-        mpegtsmux name=muxer ! filesink location=$FIFO \
-        rtspsrc location=$INPUTFILE name=src \
+        mpegtsmux name=muxer ! filesink location=$fifo \
+        rtspsrc location=$inputfile name=src \
         src. ! rtpmp4gdepay ! queue ! muxer. \
         src. ! rtph264depay ! queue ! muxer. 
-    } > "$LOG" 2>&1 &
-    TOKILL="$! $TOKILL"
-    SOURCE="$FIFO"
+    } > "$log" 2>&1 &
+    tokill="$! $tokill"
+    source="$fifo"
     ;;
   *)
     ;;
@@ -90,88 +90,88 @@ esac
 
 
 # Preprocess
-if [ "$PREPROCESS" ]; then
+if [ "$preprocess" ]; then
   echo "Starting preprocessor"
-  FIFO="$WORK/pre.fifo"
-  FIFOS="$FIFOS $FIFO"
-  LOG="$LOGS/pre.log"
-  mkfifo $FIFO
+  fifo="$work/pre.fifo"
+  fifos="$fifos $fifo"
+  log="$logs/pre.log"
+  mkfifo $fifo
   {
-    ffmpeg -vsync cfr  -y -i "$SOURCE" -r:v 25 -r:a 48000 \
+    ffmpeg -vsync cfr  -y -i "$source" -r:v 25 -r:a 48000 \
       -acodec pcm_s16le -vcodec rawvideo \
-      -f avi "$FIFO"
-  } > "$LOG" 2>&1 &
-  TOKILL="$! $TOKILL"
-  SOURCE="$FIFO"
-  PIPEFMT=avi
+      -f avi "$fifo"
+  } > "$log" 2>&1 &
+  tokill="$! $tokill"
+  source="$fifo"
+  pipefmt=avi
 fi
 
-TEES="cat '$SOURCE'"
-IDX=1
-for RT in $RATES; do
-  PFX="$OUTPUTFILE-$IDX"
-  FRAG="$PFX/%05d.ts"
+tees="cat '$source'"
+idx=1
+for rt in $rates; do
+  pfx="$outputfile-$idx"
+  frag="$pfx/%05d.ts"
 
   W=; H=; R=; BV=; BA=; P=; AR=
-  eval $RT
+  eval $rt
 
   S="${W}x${H}"
-  KEYINT=$( perl -e "print $GOP*$R" )
+  keyint=$( perl -e "print $gop*$R" )
 
-  if [ "$BURNIN" ]; then
+  if [ "$burnin" ]; then
     echo "Burnin enabled"
     # Edit the next line with care - the leading and trailing blanks are \xA0 (non-breaking space)
-    CAP=" $S ${BV}k "
-    FS=72
-    SH=2
-    STYLE="fontcolor=white:fontsize=$FS:fontfile=$FONT"
-    METRICS="shadowcolor=black@0.7:shadowx=$SH:shadowy=$SH:x=9*W/10-tw:y=8*H/10"
-    DT="drawtext=$STYLE:$METRICS:timecode='00\\:00\\:00\\:01':rate=25/1:text='$CAP'" 
+    cap=" $S ${BV}k "
+    fs=72
+    sh=2
+    style="fontcolor=white:fontsize=$fs:fontfile=$font"
+    metrics="shadowcolor=black@0.7:shadowx=$sh:shadowy=$sh:x=9*W/10-tw:y=8*H/10"
+    dt="drawtext=$style:$metrics:timecode='00\\:00\\:00\\:01':rate=25/1:text='$cap'" 
   else
-    DT="null"
+    dt="null"
   fi
 
   # Make it 16x9
-  PAD="pad=ih*16/9:ih:(ow-iw)/2:(oh-ih)/2"
+  pad="pad=ih*16/9:ih:(ow-iw)/2:(oh-ih)/2"
 
-  echo "Encoding bit rate $IDX ($S, ${BV}k)"
-  mkdir -p "$PFX"
-  FIFO="$WORK/br.$IDX.fifo"
-  LOG="$LOGS/ffmpeg.$IDX.log"
-  FIFOS="$FIFOS $FIFO"
-  TEES="$TEES | tee $FIFO"
-  mkfifo $FIFO
+  echo "Encoding bit rate $idx ($S, ${BV}k)"
+  mkdir -p "$pfx"
+  fifo="$work/br.$idx.fifo"
+  log="$logs/ffmpeg.$idx.log"
+  fifos="$fifos $fifo"
+  tees="$tees | tee $fifo"
+  mkfifo $fifo
   {
-    ffmpeg -vsync cfr -f $PIPEFMT -i "$FIFO" \
+    ffmpeg -vsync cfr -f $pipefmt -i "$fifo" \
       -map 0:0 -map 0:1 \
-      $AUDIO_OPTIONS -r:a $AR -b:a ${BA}k \
-      $VIDEO_OPTIONS -profile:v $P $VIDEO_EXTRA \
-      -g $KEYINT -keyint_min $[KEYINT/2] -r:v $R -b:v ${BV}k \
-      -s $S -vf "$PAD,$DT" \
+      $audio_options -r:a $AR -b:a ${BA}k \
+      $video_options -profile:v $P $video_extra \
+      -g $keyint -keyint_min $[keyint/2] -r:v $R -b:v ${BV}k \
+      -s $S -vf "$pad,$dt" \
       -flags -global_header -threads 0 \
-      -f segment -segment_time $GOP -segment_format mpegts \
-      "$FRAG" < /dev/null 
+      -f segment -segment_time $gop -segment_format mpegts \
+      "$frag" < /dev/null 
     echo
     echo "Exit code: $?"
-  } > "$LOG" 2>&1 &
-  IDX=$[IDX+1]
+  } > "$log" 2>&1 &
+  idx=$[idx+1]
 done
 
-TEES="$TEES > /dev/null"
+tees="$tees > /dev/null"
 
-eval $TEES &
-TOKILL="$! $TOKILL"
+eval $tees &
+tokill="$! $tokill"
 
-if [ "$LIVE" ]; then
+if [ "$live" ]; then
   echo "Starting live HLS packager"
-  perl tools/hlswrap.pl --index --gop $GOP --live "$OUTPUTDIR" &
-  TOKILL="$! $TOKILL"
+  perl tools/hlswrap.pl --index --gop $gop --live "$outputdir" &
+  tokill="$! $tokill"
   echo "Running"
   wait
 else
   wait
   echo "Packaging HLS on-demand"
-  perl tools/hlswrap.pl --index --gop $GOP "$OUTPUTDIR"
+  perl tools/hlswrap.pl --index --gop $gop "$outputdir"
 fi
 
 _cleanup
