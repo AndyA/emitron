a8ea6b9730a66b5b5f46ebd0d04145f6229dbe69
Andy Armstrong <andy@hexten.net>
2012-12-04 03:46:46 +0000

    Wrapper for Emitron scripts.

---
+++ app/lib/Emitron/Worker/Script.pm
@@ -0,0 +1,20 @@
+package Emitron::Worker::Script;
+
+use strict;
+use warnings;
+
+use Emitron::Core;
+
+=head1 NAME
+
+Emitron::Worker::Script - The worker wrapper for a script.
+
+=cut
+
+sub run {
+  my $self = shift;
+}
+
+1;
+
+# vim:ts=2:sw=2:sts=2:et:ft=perl
