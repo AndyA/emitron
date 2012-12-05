9acbc6f3efbed454e693044b199df772fd57abb7
Andy Armstrong <andy@hexten.net>
2012-12-05 22:08:54 +0000

    Rejig config a bit.

--- app/lib/Emitron/Config.pm
+++ app/lib/Emitron/Config.pm
@@ -28,133 +28,143 @@ sub config {
           crtmpserver => 'http://localhost:6502',
         },
       },
-      global  => { uri => { home => 'http://thespace.org', }, },
-      profile => {
-        t10 => {
-          v => {
-            bitrate => 200_000,
-            rate    => 25,
-            profile => 'Main L3',
-            width   => 224,
-            height  => 126
+      global => {
+        uri     => { home => 'http://thespace.org', },
+        profile => {
+          t10 => {
+            v => {
+              bitrate => 200_000,
+              rate    => 25,
+              profile => 'main',
+              level   => 3,
+              width   => 224,
+              height  => 126
+            },
+            a => {
+              bitrate => 96_000,
+              profile => 'aac_he',
+              rate    => 22_050,
+            }
           },
-          a => {
-            bitrate => 96_000,
-            profile => 'AAC_HE',
-            rate    => 22_050,
-          }
-        },
-        p10 => {
-          v => {
-            bitrate => 32_000,
-            rate    => 5,
-            profile => 'Baseline',
-            width   => 224,
-            height  => 126
+          p10 => {
+            v => {
+              bitrate => 32_000,
+              rate    => 5,
+              profile => 'baseline',
+              width   => 224,
+              height  => 126
+            },
+            a => {
+              bitrate => 24_000,
+              profile => 'aac_he',
+              rate    => 22_050,
+            }
           },
-          a => {
-            bitrate => 24_000,
-            profile => 'AAC_HE',
-            rate    => 22_050,
-          }
-        },
-        p20 => {
-          v => {
-            bitrate => 128_000,
-            rate    => 12.5,
-            profile => 'Baseline L3',
-            width   => 400,
-            height  => 224
+          p20 => {
+            v => {
+              bitrate => 128_000,
+              rate    => 12.5,
+              profile => 'baseline',
+              level   => 3,
+              width   => 400,
+              height  => 224
+            },
+            a => {
+              bitrate => 48_000,
+              profile => 'aac_lc',
+              rate    => 44_100,
+            }
           },
-          a => {
-            bitrate => 48_000,
-            profile => 'AAC-LC',
-            rate    => 44_100,
-          }
-        },
-        p30 => {
-          v => {
-            bitrate => 304_000,
-            rate    => 25,
-            profile => 'Baseline L3',
-            width   => 400,
-            height  => 224
+          p30 => {
+            v => {
+              bitrate => 304_000,
+              rate    => 25,
+              profile => 'baseline',
+              level   => 3,
+              width   => 400,
+              height  => 224
+            },
+            a => {
+              bitrate => 64_000,
+              profile => 'aac_lc',
+              rate    => 44_100,
+            }
           },
-          a => {
-            bitrate => 64_000,
-            profile => 'AAC-LC',
-            rate    => 44_100,
-          }
-        },
-        p40 => {
-          v => {
-            bitrate => 400_000,
-            rate    => 25,
-            profile => 'Main L3',
-            width   => 512,
-            height  => 288
+          p40 => {
+            v => {
+              bitrate => 400_000,
+              rate    => 25,
+              profile => 'main',
+              level   => 3,
+              width   => 512,
+              height  => 288
+            },
+            a => {
+              bitrate => 96_000,
+              profile => 'aac_lc',
+              rate    => 44_100,
+            }
           },
-          a => {
-            bitrate => 96_000,
-            profile => 'AAC-LC',
-            rate    => 44_100,
-          }
-        },
-        p50 => {
-          v => {
-            bitrate => 700_000,
-            rate    => 25,
-            profile => 'Main L3',
-            width   => 640,
-            height  => 360
+          p50 => {
+            v => {
+              bitrate => 700_000,
+              rate    => 25,
+              profile => 'main',
+              level   => 3,
+              width   => 640,
+              height  => 360
+            },
+            a => {
+              bitrate => 96_000,
+              profile => 'aac_lc',
+              rate    => 44_100,
+            }
           },
-          a => {
-            bitrate => 96_000,
-            profile => 'AAC-LC',
-            rate    => 44_100,
-          }
-        },
-        p60 => {
-          v => {
-            bitrate => 1200_000,
-            rate    => 25,
-            profile => 'Main L3',
-            width   => 704,
-            height  => 396
+          p60 => {
+            v => {
+              bitrate => 1200_000,
+              rate    => 25,
+              profile => 'main',
+              level   => 3,
+              width   => 704,
+              height  => 396
+            },
+            a => {
+              bitrate => 96_000,
+              profile => 'aac_lc',
+              rate    => 44_100,
+            }
           },
-          a => {
-            bitrate => 96_000,
-            profile => 'AAC-LC',
-            rate    => 44_100,
-          }
-        },
-        p70 => {
-          v => {
-            bitrate => 2016_000,
-            rate    => 25,
-            profile => 'Main L3.1',
-            width   => 1024,
-            height  => 576
+          p70 => {
+            v => {
+              bitrate => 2016_000,
+              rate    => 25,
+              profile => 'main',
+              level   => 3.1,
+              width   => 1024,
+              height  => 576
+            },
+            a => {
+              bitrate => 96_000,
+              profile => 'aac_lc',
+              rate    => 44_100,
+            }
           },
-          a => {
-            bitrate => 96_000,
-            profile => 'AAC-LC',
-            rate    => 44_100,
-          }
-        },
-        p80 => {
-          v => {
-            bitrate => 3372_000,
-            rate    => 25,
-            profile => 'High L4',
-            width   => 1280,
-            height  => 720
+          p80 => {
+            v => {
+              bitrate => 3372_000,
+              rate    => 25,
+              profile => 'high',
+              level   => 4,
+              width   => 1280,
+              height  => 720
+            },
+            a => {
+              bitrate => 128_000,
+              profile => 'aac_lc',
+              rate    => 44_100,
+            }
           },
-          a => {
-            bitrate => 128_000,
-            profile => 'AAC-LC',
-            rate    => 44_100,
-          }
         },
       },
     },
