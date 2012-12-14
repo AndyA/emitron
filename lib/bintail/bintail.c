bb44fa95610a26bd402d196a93db6e7678b2dbc7
Andy Armstrong <andy@hexten.net>
2012-12-14 14:52:28 +0000

    bintail initial checkin.

---
+++ lib/bintail/bintail.c
@@ -0,0 +1,16 @@
+/* Binary Tail.
+ *
+ * Follow a growing binary file starting at any offset
+ * Follow a sequentially named series of files
+ *
+ * bintail.c */
+
+#include <stdio.h>
+
+int
+main(void) {
+  return 0;
+}
+
+/* vim:ts=2:sw=2:sts=2:et:ft=c
+ */
