5b61210bd8a82d4a8b2ab243df66edb8416e1549
Andy Armstrong <andy@hexten.net>
2012-12-15 03:39:41 +0000

    Seems to work.

--- lib/fatcat/buffer.c
+++ lib/fatcat/buffer.c
@@ -1,7 +1,6 @@
 /* buffer.c */
 
 #include <stdio.h>
-#include <pthread.h>
 
 #include "utils.h"
 #include "buffer.h"
@@ -10,6 +9,9 @@ buffer *b_new(size_t sz) {
   buffer *b = alloc(sizeof(buffer));
   b->size = sz + 1; // +1 byte because we don't want to ever be full
   b->b = alloc(b->size);
+  pthread_mutex_init(&b->mutex, NULL);
+  pthread_cond_init(&b->can_read, NULL);
+  pthread_cond_init(&b->can_write, NULL);
   return b;
 }
 
@@ -27,6 +29,7 @@ void b_free(buffer *b) {
 
 buffer_reader *b_add_reader(buffer *b) {
   buffer_reader *br = alloc(sizeof(buffer_reader));
+  br->b = b;
   br->next = b->br;
   b->br = br;
   return br;
@@ -54,40 +57,86 @@ static size_t _b_get_buf(const buffer *b, buffer_iov *bi,
   return avail;
 }
 
-size_t b_space(const buffer *b) {
+size_t b_space(buffer *b) {
   buffer_reader *br;
   size_t space = 0;
+  /*  pthread_mutex_lock(&b->mutex);*/
   for (br = b->br; br; br = br->next) {
     ssize_t sp = br->pos - b->pos;
     if (sp <= 0) sp += b->size;
     if (space == 0 || sp < space) space = sp;
   }
+  /*  pthread_mutex_unlock(&b->mutex);*/
   if (space > 0) return space - 1;
   return 0;
 }
 
-size_t b_available(const buffer *b, const buffer_reader *br) {
+size_t b_available(buffer_reader *br) {
+  buffer *b = br->b;
+  /*  pthread_mutex_lock(&b->mutex);*/
   ssize_t avail = b->pos - br->pos;
   if (avail < 0) avail += b->size;
+  /*  pthread_mutex_unlock(&b->mutex);*/
   return avail;
 }
 
-size_t b_get_input(const buffer *b, buffer_iov *bi) {
+size_t b_get_input(buffer *b, buffer_iov *bi) {
   return _b_get_buf(b, bi, b_space(b), b->pos);
 }
 
-size_t b_get_output(const buffer *b, const buffer_reader *br, buffer_iov *bi) {
-  return _b_get_buf(b, bi, b_available(b, br), br->pos);
+size_t b_get_output(buffer_reader *br, buffer_iov *bi) {
+  return _b_get_buf(br->b, bi, b_available(br), br->pos);
 }
 
 void b_commit_input(buffer *b, size_t len) {
-  b->pos += len;
-  if (b->pos >= b->size) b->pos -= b->size;
+  if (len) {
+    pthread_mutex_lock(&b->mutex);
+    b->pos += len;
+    if (b->pos >= b->size) b->pos -= b->size;
+    pthread_mutex_unlock(&b->mutex);
+    pthread_cond_broadcast(&b->can_read);
+  }
 }
 
-void b_commit_output(const buffer *b, buffer_reader *br, size_t len) {
-  br->pos += len;
-  if (br->pos >= b->size) br->pos -= b->size;
+void b_commit_output(buffer_reader *br, size_t len) {
+  if (len) {
+    buffer *b = br->b;
+    pthread_mutex_lock(&b->mutex);
+    br->pos += len;
+    if (br->pos >= b->size) br->pos -= b->size;
+    pthread_mutex_unlock(&b->mutex);
+    pthread_cond_broadcast(&b->can_write);
+  }
+}
+
+size_t b_wait_input(buffer *b, buffer_iov *bi) {
+  size_t sz = 0;
+  pthread_mutex_lock(&b->mutex);
+  for (;;) {
+    sz = b_get_input(b, bi);
+    if (sz) break;
+    pthread_cond_wait(&b->can_write, &b->mutex);
+  }
+  pthread_mutex_unlock(&b->mutex);
+  return sz;
+}
+
+size_t b_wait_output(buffer_reader *br, buffer_iov *bi) {
+  buffer *b = br->b;
+  size_t sz = 0;
+  pthread_mutex_lock(&b->mutex);
+  for (;;) {
+    sz = b_get_output(br, bi);
+    if (sz || b->eof) break;
+    pthread_cond_wait(&b->can_read, &b->mutex);
+  }
+  pthread_mutex_unlock(&b->mutex);
+  return sz;
+}
+
+void b_eof(buffer *b) {
+  b->eof = 1;
+  pthread_cond_broadcast(&b->can_read);
 }
 
 /* vim:ts=2:sw=2:sts=2:et:ft=c
--- lib/fatcat/fatcat.c
+++ lib/fatcat/fatcat.c
@@ -1,10 +1,11 @@
 /* Fat Cat
  */
 
+#include <errno.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
-#include <errno.h>
+#include <string.h>
 
 #include <getopt.h>
 
@@ -13,25 +14,13 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#include "buffer.h"
 #include "utils.h"
+#include "buffer.h"
 
 #define BUFSIZE (1024 * 1024)
 
-static int verbose = 0;
-static int decouple = 0;
 static size_t bufsize = BUFSIZE;
 static const char *infile;
-
-static void mention(const char *msg, ...) {
-  if (verbose) {
-    va_list ap;
-    va_start(ap, msg);
-    vfprintf(stderr, msg, ap);
-    fprintf(stderr, "\n");
-    va_end(ap);
-  }
-}
 
 static size_t get_size(const char *opt) {
   ssize_t sz = parse_size(opt);
@@ -44,9 +33,7 @@ static void usage(const char *prog) {
           "Options:\n"
           "  -i             <file>     Input file\n"
           "  -b             <size>     Buffer size\n"
-          "  -d, --decouple            Use a buffer for each writer\n"
           "  -h, --help                See this text\n"
-          "  -v, --verbose             Verbose output\n"
           "\n", prog);
   exit(1);
 }
@@ -56,9 +43,7 @@ static void parse_options(int *argc, char ***argv) {
   int ch, oidx;
 
   static struct option opts[] = {
-    {"decouple", no_argument, NULL, 'd'},
     {"help", no_argument, NULL, 'h'},
-    {"verbose", no_argument, NULL, 'v'},
     {NULL, 0, NULL, 0}
   };
 
@@ -70,33 +55,91 @@ static void parse_options(int *argc, char ***argv) {
     case 'b':
       bufsize = get_size(optarg);
       break;
-    case 'd':
-      decouple++;
-      break;
     case 'h':
     default:
       usage(prog);
-    case 'v':
-      verbose++;
-      break;
     }
   }
 
   *argc -= optind;
   *argv += optind;
+}
 
-  if (*argc == 0) {
-    usage(prog);
+struct wtr_ctx {
+  buffer_reader *br;
+  int fd;
+  pthread_t t;
+};
+
+static void *writer(void *ctx) {
+  struct wtr_ctx *cx = (struct wtr_ctx *) ctx;
+  for (;;) {
+    buffer_iov iov;
+    size_t spc = b_wait_output(cx->br, &iov);
+    if (spc == 0) break;
+    ssize_t put = writev(cx->fd, iov.iov, iov.iovcnt);
+    if (put < 0) die("I/O error: %s", strerror(errno));
+    b_commit_output(cx->br, put);
   }
+  return NULL;
+}
+
+static struct wtr_ctx *add_writer(buffer *b, int fd) {
+  struct wtr_ctx *ctx = alloc(sizeof(struct wtr_ctx));
+  ctx->br = b_add_reader(b);
+  ctx->fd = fd;
+  pthread_create(&ctx->t, NULL, writer, ctx);
+  return ctx;
 }
 
 static void fatcat(int nfile, char *file[]) {
-  printf("%lu\n", bufsize);
+  buffer *b = b_new(bufsize);
+  struct wtr_ctx **ws = alloc(nfile * sizeof(struct wtr_ctx *));
+  int wi = 0, i;
+  int ifd = 0;
+
+  if (infile) {
+    ifd = open(infile, O_RDONLY);
+    if (ifd < 0) die("Can't read %s: %s", infile, strerror(errno));
+  }
+
+  if (nfile) {
+    while (nfile) {
+      int fd = open(file[wi], O_WRONLY | O_CREAT, 0666);
+      if (fd < 0) die("Can't write %s: %s", file[wi], strerror(errno));
+      ws[wi++] = add_writer(b, fd);
+      nfile--;
+    }
+  }
+  else {
+    ws[wi++] = add_writer(b, 1); // stdout
+  }
+
+  for (;;) {
+    buffer_iov iov;
+    b_wait_input(b, &iov);
+    ssize_t got = readv(ifd, iov.iov, iov.iovcnt);
+    if (got < 0) die("I/O error: %s", strerror(errno));
+    if (got == 0) break;
+    b_commit_input(b, got);
+  }
+
+  b_eof(b);
+
+  for (i = 0; i < wi; i++) {
+    void *rv;
+    pthread_join(ws[i]->t, &rv);
+    if (ws[i]->fd > 2) close(ws[i]->fd);
+  }
+
+  if (ifd > 2) close(ifd);
+  b_free(b);
 }
 
 int main(int argc, char *argv[]) {
   parse_options(&argc, &argv);
   fatcat(argc, argv);
+  pthread_exit(NULL);
   return 0;
 }
 
