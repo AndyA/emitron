493e4bbd94aadfffd9214645a01ccb392af2b7f2
Andy Armstrong <andy@hexten.net>
2013-02-14 19:52:53 +0100

    Encode ACE assets.

---
+++ tools/ace.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+out="webroot/ace"
+mkdir -p "$out"
+for f in ~/ACE/raw/*.mpg; do
+  name="$( basename "$f" ".mpg" )"
+  outd="$out/$name"
+  echo "$f $outd"
+  if [ -d "$outd" ]; then
+    echo "$outd exists, skipping"
+  else
+    echo "Encoding $f to $outd"
+    ./tools/hlsvod.sh -p "$f" "$outd"
+  fi
+done
+
+# vim:ts=2:sw=2:sts=2:et:ft=sh
+
