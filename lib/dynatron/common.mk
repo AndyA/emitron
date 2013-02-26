8d905aa5ef6101021e989c7aadd20ce466f5dd6a
Andy Armstrong <andy@hexten.net>
2013-02-26 01:09:22 +0000

    Add some options to common makefile.

--- lib/dynatron/common.mk
+++ lib/dynatron/common.mk
@@ -1,3 +1,16 @@
 CFLAGS=-Wall -Werror -g2 -D_LARGEFILE64_SOURCE --std=c99
 LDFLAGS=-lc -lm
 
+ifneq ($(shell $(CC) -v 2>&1 | grep -i clang),)
+CFLAGS+=-Qunused-arguments
+endif
+
+ifneq ($(COVER),)
+CFLAGS+=-fprofile-arcs -ftest-coverage
+LDFLAGS+=-lgcov
+endif
+
+ifneq ($(PROFILE),)
+CFLAGS+=-pg
+LDFLAGS+=-pg
+endif
