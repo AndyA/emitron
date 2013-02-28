a9ffc7de53a037e139457a25440490c197364fc7
Andy Armstrong <andy@hexten.net>
2013-02-28 17:52:40 +0000

    Seems to work without -s. Boggle.

--- mxf/tools/cym.sh
+++ mxf/tools/cym.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #in="wales/WG0001323_PEET034K.mxf"
+bars="bars-mpeg.mov"
 in="foo.mov"
 out="part-out.mov"
 
@@ -8,7 +9,6 @@ ffmpeg -y -i "$in" \
   -vf format=yuv420p,yadif,crop=720:576:0:32 \
   -c:a libfaac -b:a 192k \
   -c:v libx264 -b:v 5000k \
-  -s 1024x576 \
   -aspect 16:9 \
   "$out"
 
