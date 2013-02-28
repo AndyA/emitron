6dd6a59d2d020eeefb051ef49168de3ded356398
Andy Armstrong <andy@hexten.net>
2013-02-28 08:59:43 +0000

    Send full URL of assets.

--- webroot/barcode/svc/barcode.php
+++ webroot/barcode/svc/barcode.php
@@ -1,19 +1,21 @@
 <?php
   header('Content-type: application/json');
-  $base = '../ace';
+  $self = (empty($_SERVER['HTTPS']) ? 'http' : 'https')
+    . '://' . $_SERVER['SERVER_NAME'] . dirname($_SERVER['REQUEST_URI']);
+  $base = "../../ace";
   $db = array();
-  chdir(dirname(__FILE__) . "/../$base");
+  chdir(dirname(__FILE__) . "/$base");
   $thumbs = glob("*/frames/barcode-thumb.jpeg");
   foreach ($thumbs as $tb) {
     $name = dirname(dirname($tb));
     array_push($db, array(
-      name  => $name, 
-      base  => "$base/", 
-      thumb => "$tb", 
-      media => "$name/$name.m3u8", 
-      full  => "$name/frames/barcode.jpeg", 
-      raw   => "$name/frames/barcode-raw.jpeg",
-      data  => "data/$name.json"
+      'name'  => $name, 
+      'base'  => "$self/$base/", 
+      'thumb' => "$tb", 
+      'media' => "$name/$name.m3u8", 
+      'full'  => "$name/frames/barcode.jpeg", 
+      'raw'   => "$name/frames/barcode-raw.jpeg",
+      'data'  => "data/$name.json"
     ));
   }
   print json_encode($db);
