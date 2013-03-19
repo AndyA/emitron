8295bd15d6cb5cc66cd989202dbbde463c87d549
Andy Armstrong <andy@hexten.net>
2013-03-19 16:35:14 +0000

    Wait for a file to exist.

---
+++ lintilla/app/lib/Lintilla/Util.pm
@@ -0,0 +1,31 @@
+package Lintilla::Util;
+
+use strict;
+use warnings;
+
+use Time::HiRes qw( sleep time );
+
+use base qw( Exporter );
+
+our @EXPORT_OK = qw( wait_for_file );
+our %EXPORT_TAGS = ( all => [@EXPORT_OK] );
+
+=head1 NAME
+
+Lintilla::Util - Utility stuff
+
+=cut
+
+sub wait_for_file {
+  my ( $name, $timeout ) = @_;
+  my $deadline = defined $timeout ? time + $timeout : undef;
+  until ( defined $deadline && time >= $deadline ) {
+    return $name if -e $name;
+    sleep 0.1;
+  }
+  return;
+}
+
+1;
+
+# vim:ts=2:sw=2:sts=2:et:ft=perl
