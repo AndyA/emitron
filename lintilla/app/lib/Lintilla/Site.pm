6c334a3573c17d00536fe41aad557f20e6a93c1d
Andy Armstrong <andy@hexten.net>
2013-03-19 15:27:13 +0000

    Test spodge.

--- lintilla/app/lib/Lintilla/Site.pm
+++ lintilla/app/lib/Lintilla/Site.pm
@@ -4,7 +4,16 @@ use Dancer ':syntax';
 our $VERSION = '0.1';
 
 get '/' => sub {
-    template 'index';
+  template 'index';
+};
+
+get '/app/helper' => sub {
+  content_type 'text/plain';
+  return "Hello, World!";
+};
+
+get '/app/404' => sub {
+  template 'index';
 };
 
 true;
