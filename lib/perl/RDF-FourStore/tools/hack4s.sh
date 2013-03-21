3d3a84981d3afdf16e23de6f1dae011f0148342e
Andy Armstrong <andy@hexten.net>
2013-03-21 17:08:02 +0000

    Variable name.

--- lib/perl/RDF-FourStore/tools/hack4s.sh
+++ lib/perl/RDF-FourStore/tools/hack4s.sh
@@ -4,12 +4,12 @@ bindir="bin4s"
 tmpdir="/tmp/4s.tmp"
 mkdir -p "$bindir"
 
-fs_backend_setup=$( which 4s-backend ) || {
+fs_backend=$( which 4s-backend ) || {
   echo "Can't find 4s-backend" 1>&2
   exit 1
 }
 
-datadir="$( strings "$fs_backend_setup" | \
+datadir="$( strings "$fs_backend" | \
   perl -lne 'print $1 if m{^(/.+)/%s/metadata.nt}' )" || {
   echo "Can't find data directory" 1>&2
   exit 1
@@ -19,13 +19,14 @@ while [ ${#tmpdir} -lt ${#datadir} ]; do tmpdir="${tmpdir}_"; done
 
 mkdir -p "$tmpdir"
 
-fs_dir="$( dirname "$fs_backend_setup" )"
+fs_dir="$( dirname "$fs_backend" )"
 find "$fs_dir" -maxdepth 1 -name '4s-*' | while read bin; do
   dst="$bindir"/"$( basename "$bin" )"
   [ "$bin" -nt "$dst" ] && perl -pe "s@$datadir@$tmpdir@g" < "$bin" > "$dst"
   chmod a+x "$dst"
 done
 
+echo "$tmpdir";
 
 # vim:ts=2:sw=2:sts=2:et:ft=sh
 
