383620724dc624020be40fe7f154a6d3cf29d9ff
Andy Armstrong <andy@hexten.net>
2013-08-13 13:27:50 +0100

    Snapshot.

--- r5live/encoder/play_aac.sh
+++ r5live/encoder/play_aac.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -x
+#ffmpeg -re -y -ss 65 -t 43 -i "$ENC_SOURCE" -c copy "$ENC_FIFO" < /dev/null > "$ENC_LOG" 2>&1
 ffmpeg -re -y -i "$ENC_SOURCE" -c copy "$ENC_FIFO" < /dev/null > "$ENC_LOG" 2>&1
 
 # vim:ts=2:sw=2:sts=2:et:ft=sh
