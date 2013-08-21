e7760e6122f924ef262ac9393731339c36ea594d
Andy Armstrong <andy@hexten.net>
2013-08-21 23:56:19 +0100

    All fragments: 8s.

--- nginx/tools/enc1.sh
+++ nginx/tools/enc1.sh
@@ -3,13 +3,13 @@
 in="$1"
 out="$2"
 
-/opt/ffmpeg/bin/ffmpeg -i "$in" \
+exec /opt/ffmpeg/bin/ffmpeg -i "$in" \
   -force_key_frames "expr:gte(t,n_forced*8)" \
   -c:v libx264 -vprofile main \
   -s 400x224 \
   -b:v 350k \
   -c:a libfaac -ar 44100 -ac 2 -b:a 96k \
-  -f flv "$out" 2>> logs/ffmpeg-hls1.log
+  -f flv "$out" < /dev/null 2>> logs/ffmpeg-hls1.log
 
 # vim:ts=2:sw=2:sts=2:et:ft=sh
 
--- nginx/tools/enc2.sh
+++ nginx/tools/enc2.sh
@@ -3,13 +3,13 @@
 in="$1"
 out="$2"
 
-/opt/ffmpeg/bin/ffmpeg -i "$in" \
+exec /opt/ffmpeg/bin/ffmpeg -i "$in" \
   -force_key_frames "expr:gte(t,n_forced*8)" \
   -c:v libx264 -vprofile main \
   -s 640x360 \
   -b:v 800k \
   -c:a libfaac -ar 44100 -ac 2 -b:a 128k \
-  -f flv "$out" 2>> logs/ffmpeg-hls2.log
+  -f flv "$out" < /dev/null 2>> logs/ffmpeg-hls2.log
 
 # vim:ts=2:sw=2:sts=2:et:ft=sh
 
