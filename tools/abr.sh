cf774e6d2032eb36406a349743914fd6957987e0
Andy Armstrong <andy@hexten.net>
2013-09-05 15:58:16 +0100

    Make abr encoder work more like the live chain.

--- tools/abr.sh
+++ tools/abr.sh
@@ -3,26 +3,24 @@
 INPUTFILE="$1"
 OUTPUTDIR="$2"
 if [ -z "$OUTPUTDIR" ]; then
-  echo "Usage: $0 <infile> <outfile>" 1>&2
+  echo "Usage: $0 <infile> <outdir>" 1>&2
   exit 1
 fi
 
 OUTPUTFILE="$OUTPUTDIR/$( basename "$OUTPUTDIR" )"
 
 GOP=8
-PRESET=veryfast
 [ -z "$BURNIN" ] && BURNIN=true
 AUDIO_OPTIONS="-acodec libfaac -ac 2"
 VIDEO_OPTIONS="-vcodec libx264"
-VIDEO_EXTRA="-preset $PRESET -sc_threshold 0"
 
 FONT="$HOME/Dropbox/Fonts/Envy Code R.ttf"
 
 RATES="
   W=400;H=224;R=25;BV=300;BA=96;AR=44100;P=baseline
-  W=640;H=360;R=25;BV=704;BA=96;AR=44100;P=main
-  W=688;H=384;R=25;BV=1372;BA=128;AR=48000;P=main
-  W=1024;H=576;R=25;BV=2000;BA=96;AR=44100;P=main"
+  W=640;H=360;R=25;BV=704;BA=128;AR=44100;P=main
+  W=1024;H=576;R=25;BV=1500;BA=128;AR=44100;P=main
+  W=1280;H=720;R=25;BV=2200;BA=192;AR=44100;P=main"
 
 FIFOS=""
 TEES=""
@@ -40,12 +38,12 @@ set -x
 for RT in $RATES; do
   PFX="$OUTPUTFILE-$IDX"
   FRAG="$PFX/%05d.ts"
+  M3U8="$PFX/m.m3u8"
 
   W=; H=; R=; BV=; BA=; P=; AR=
   eval $RT
 
   S="${W}x${H}"
-  KEYINT=$( perl -e "print $GOP*$R" )
 
   if $BURNIN; then
     # Edit the next line with care - the leading and trailing blanks are \xA0 (non-breaking space)
@@ -59,7 +57,7 @@ for RT in $RATES; do
     DT="null"
   fi
 
-  mkdir -p "$PFX"
+  mkdir -p "$( dirname "$FRAG" )"
   FIFO="/tmp/hlslive.$$.$IDX.fifo"
   FIFOS="$FIFOS $FIFO"
   TEES="$TEES | tee $FIFO"
@@ -67,10 +65,16 @@ for RT in $RATES; do
   ffmpeg -y -f mpegts -i "$FIFO" -vf "$DT" \
     -map 0:0 -map 0:1 \
     $AUDIO_OPTIONS -ar $AR -b:a ${BA}k \
-    $VIDEO_OPTIONS -profile $P $VIDEO_EXTRA \
-    -g $KEYINT -keyint_min $[KEYINT/2] -r $R -b:v ${BV}k -s $S \
+    $VIDEO_OPTIONS -profile $P \
+    -force_key_frames "expr:gte(t,n_forced*$GOP)" \
+    -r $R -b:v ${BV}k -s $S \
     -flags -global_header -threads 0 \
-    -f mpegts "$PFX.ts" < /dev/null &
+    -f segment \
+    -segment_time $GOP \
+    -segment_format mpegts \
+    -segment_list "$M3U8" \
+    -segment_list_type m3u8 \
+    "$FRAG" < /dev/null &
 
   IDX=$[IDX+1]
 
