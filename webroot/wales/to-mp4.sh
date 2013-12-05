8cb81c0c5bf37454f6196432f7399a3deea7e3af
Andy Armstrong <andy@hexten.net>
2013-12-05 12:02:03 +0000

    Convert to mp4.

---
+++ webroot/wales/to-mp4.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+out="mp4"
+
+set -x
+mkdir -p "$out"
+
+find a b -name '*.m3u8' | while read mf; do
+  echo "$mf"
+  mn="$( basename "$mf" .m3u8 )"
+  [ "$mn" = "a" ] && mn="$( basename "$( dirname "$mf" )" )"
+  outf="$PWD/$out/$mn.mp4"
+  echo "$mn $outf"
+  pushd "$( dirname "$mf" )"
+  ffmpeg -y -i "$( basename "$mf" )" \
+    -af aresample=osr=44100:filter_size=256:cutoff=1 \
+    -c:v copy -c:a libfaac -b:a 128k "$outf" < /dev/null
+  popd
+done
+
+# vim:ts=2:sw=2:sts=2:et:ft=sh
+
